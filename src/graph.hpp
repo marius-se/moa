229abb1d4ed911022b88397565b53ceef5261b3c
Marius Seufzer <marius.seufzer@protonmail.com>
2023-11-06 19:38:30 +1300

    fix bfs cpu implementation

--- src/graph.hpp
+++ src/graph.hpp
@@ -2,13 +2,14 @@
 #define GRAPH
 
 #include <string>
+#include <vector>
 
 class Graph {
   public:
     Graph(int vertices[], const int numberOfVertices, int edges[], const int numberOfEdges);
     Graph(const std::string csvFilePath, const bool reverseColumns, const int numberOfVertices, const int numberOFEdges);
     void bfs(int startVertex);
-    void bfsCPU(int startVertex);
+    std::vector<int> bfsCPU(int startVertex);
     ~Graph();
     const int numberOfVertices;
     const int numberOfEdges;
--- tests/test_graph.cpp
+++ tests/test_graph.cpp
@@ -1,13 +1,14 @@
 #include <catch2/catch_test_macros.hpp>
 #include <cstdio>
 #include <graph.hpp>
+#include <vector>
 
 TEST_CASE("Construct Graph from CSV file") {
     constexpr int expectedNumberOfEdges = 12;
     constexpr int expectedEdgesArraySize = 19; // 12 edges + 7 seperators;
     constexpr int expectedNumberOfVertices = 7;
-    int expectedVertices[] = { 0, 4, 7, 9, 13, 18, 16 };
-    int expectedEdges[] = { 1, 2, 3, -1, 3, 4, -1, 5, -1, 2, 5, 6, -1, 3, 6, -1, 5, -1, -1 };
+    constexpr int expectedVertices[] = { 0, 4, 7, 9, 13, 18, 16 };
+    constexpr int expectedEdges[] = { 1, 2, 3, -1, 3, 4, -1, 5, -1, 2, 5, 6, -1, 3, 6, -1, 5, -1, -1 };
 
     Graph graph("./tests/test_csv.csv", false, expectedNumberOfVertices, expectedNumberOfEdges);
 
@@ -23,4 +24,12 @@ TEST_CASE("Construct Graph from CSV file") {
     for (int i = 0; i < expectedEdgesArraySize; ++i) {
         REQUIRE(graph.edges[i] == expectedEdges[i]);
     }
+}
+
+TEST_CASE("Calculate BFS using CPU") {
+    Graph graph("./tests/test_csv.csv", false, 7, 12);
+    
+    std::vector<int> expectedCosts { 0, 1, 1, 1, 2, 2, 2 };
+    std::vector<int> costs = graph.bfsCPU(0);
+    REQUIRE(expectedCosts == costs);
 }
