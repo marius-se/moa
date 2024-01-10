5a0f60d7c69f955e744ed0e76c3a9c33f1a0fecd
Marius Seufzer <marius.seufzer@protonmail.com>
2024-01-10 15:17:51 +1300

    improve tests

--- src/graph.hpp
+++ src/graph.hpp
@@ -7,8 +7,8 @@
 class Graph {
   public:
     Graph(std::vector<int> vertices, std::vector<int> edges);
-    Graph(const std::string csvFilePath, const bool reverseColumns, const int numberOfVertices,
-          const int numberOFEdges);
+    Graph(const std::string csvFilePath, const bool reverseColumns, const size_t numberOfVertices,
+          const size_t numberOFEdges);
     std::vector<int> bfsCPU(int startVertex);
     std::vector<int> bfsGPU(int startVertex);
     ~Graph();
